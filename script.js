a9779ba21a8781635868df7322a64cad1ffbe47e
azertyup72 <83078900+azertyup72@users.noreply.github.com>
2023-10-06 15:25:09 +0200

    Demo added

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert("brug")
+const mainMenu = document.getElementById("menu")
+const background = document.getElementById("background")
+
+function go(){
+    mainMenu.style.opacity = "0"
+    background.style.opacity = "0"
+}
