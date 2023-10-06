c81033830789456cd2835280e6af7b793026c605
azertyup72 <83078900+azertyup72@users.noreply.github.com>
2023-10-06 16:35:03 +0200

    demo updated

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const mainMenu = document.getElementById("menu")
 const background = document.getElementById("background")
 
+// launches the app
 function go(){
     mainMenu.style.opacity = "0"
     background.style.opacity = "0"
+    setTimeout(function(){
+        mainMenu.remove()
+    }, 1000)
 }
