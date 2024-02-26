200ff6ca471a938ec9d8eb350ac5f2808438f42f
azertyup72 <83078900+azertyup72@users.noreply.github.com>
2024-02-26 18:52:49 +0100

    dsqds

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ let elements = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13]
 
 // launches the app
 function go(){
-    if(document.readyState == "complete"){
+    if(document.readyState === "complete"){
         mainMenu.style.opacity = "0"
         setTimeout(function(){
             mainMenu.remove()
